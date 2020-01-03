9134438cab7077c57e3b081ce291f612fe8fabb3
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2020-01-03 16:39:47 +0200

    Add project files.

---
+++ Tasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+   public static class Program
+    {
+       public static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+            {
+                for (int x = 0; x < 1000; x++)
+                {
+                    Console.WriteLine(x);
+                }
+            });
+            t.Wait();
+            Console.WriteLine();
+            Console.WriteLine("Press any button to exit");
+            Console.ReadLine();
+        }
+    }
+}
